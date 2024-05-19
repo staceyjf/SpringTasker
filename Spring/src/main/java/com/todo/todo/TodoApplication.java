f28b70faf25458899f0773eb58dbdaa00bae18fd
staceyjf <staceyfanner@gmail.com>
2024-05-19 15:43:56 +1000

    working Log4j2 add

--- Spring/src/main/java/com/todo/todo/TodoApplication.java
+++ Spring/src/main/java/com/todo/todo/TodoApplication.java
@@ -2,12 +2,18 @@ package com.todo.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 public class TodoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
-	}
+	private static final Logger logger = LogManager.getLogger(TodoApplication.class);
 
+	public static void main(String[] args) {
+		// instructs JVM to use Log3j2's LogManager for any log messages
+		System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
+		SpringApplication.run(TodoApplication.class, args);
+		logger.info("Todo Application has successfully loaded");
+	}
 }
