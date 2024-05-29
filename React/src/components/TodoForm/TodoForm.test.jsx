1cc34fb1b37ea2f84faa5202e9ef4fdd7af82171
staceyjf <staceyfanner@gmail.com>
2024-05-30 09:42:10 +1000

    working form testing

--- React/src/components/TodoForm/TodoForm.test.jsx
+++ React/src/components/TodoForm/TodoForm.test.jsx
@@ -4,38 +4,30 @@ import { useForm } from "react-hook-form";
 import TodoForm from "./TodoForm";
 
 describe("TodoForm", () => {
-  const handleFormSubmit = vi.fn((fn) => fn); //mock up of a function
+  const onSubmit = vi.fn((fn) => fn); //mock up of a function
   const defaultValues = {
+    title: "Tester title",
     task: "Tester task",
     dueDate: new Date().toISOString(),
     isComplete: false,
     colourId: "1",
   };
 
-  const useFormMock = () => ({
-    control: {}, // mock control object
-    formState: { errors: {} }, // mock formState object
-  });
-
-  const TodoFormWrapper = (props) => {
-    const formMethods = useFormMock();
-    return <TodoForm {...props} control={formMethods.control} />;
-  };
-
-  it("should display the input of the user", () => {
+  beforeEach(() => {
     render(
-      <TodoFormWrapper
-        handleFormSubmit={handleFormSubmit}
+      <TodoForm
         defaultValues={defaultValues}
+        onSubmit={onSubmit}
         mode="Create"
       />
     );
+  });
 
-    // Add assertions here to check that the user input is displayed correctly
-    it("should return the user input for the title text field", async () => {
-      const input = await screen.findById("titleInput");
-      await userEvent.type(input, "My new title");
-      expect(input).toHaveValue("My new title");
-    });
+  // Add assertions here to check that the user input is displayed correctly
+  it("should return the user input for the title text field", async () => {
+    const input = await screen.findByLabelText("Title");
+    const user = userEvent.setup();
+    await user.type(input, "My new title");
+    expect(input).toHaveValue("My new title");
   });
 });
